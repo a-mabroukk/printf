5c5e73369d69230907f00c17ef5a067172f2451d
Alaa Fayed <alaafayed810@gmail.com>
2023-09-18 22:03:10 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/*_printf.c*/
+int _printf(const char *format, ...);
+
+/*_putchar.c function*/
+int _putchar(char c);
+
+/* print_function.c */
+int print_str(va_list list);
+int print_int(va_list list);
+
+#endif
