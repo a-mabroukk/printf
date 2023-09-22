1638454944094601961bb966f99fa0c4d4ef9c89
loda26 <mokhaled26498@gmail.com>
2023-09-22 14:57:29 -0700

    print_rev

--- print_rev.c
+++ print_rev.c
@@ -9,7 +9,7 @@
 int print_rev(va_list list)
 {
 	char *string;
-	int len, c;
+	int len, c, str_count = 0;
 
 	string = va_arg(list, char *);
 	for (len = 0; *string != '\0'; string++)
@@ -20,6 +20,8 @@ int print_rev(va_list list)
 	for (c = len; c > 0; c--)
 	{
 		_putchar(*string);
+		str_count++;
 		string--;
 	}
+	return (str_count);
 }
