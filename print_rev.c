213f8864d68378d1257596f946fbe780d4da7d6c
Alaa Fayed <alaafayed810@gmail.com>
2023-09-23 00:31:23 +0200

    reversing

---
+++ print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - print a reversed string
+ *
+ * @list: the list of arguments
+ *
+ * Return: value
+ */
+int print_rev(va_list list)
+{
+	char *string;
+	int len, c;
+
+	string = va_arg(list, char *);
+	for (len = 0; *string != '\0'; string++)
+	{
+		++len;
+	}
+	string--;
+	for (c = len c > 0; c--)
+	{
+		_putchar(*string);
+		string--;
+	}
+}
